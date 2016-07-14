95f659518ae6d2c3729037b3d91e49585788bd6a
Jason Shiverick <jshiverick@tesla.com>
2016-07-13 19:16:18 -0700

    updated start-client.sh

--- start-client.sh
+++ start-client.sh
@@ -1,10 +1,12 @@
 
 
-NAME='all-spark-notebook'
-PORT=$1
+NAME="all-spark-notebook"
+#PORT=$1
 HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 docker stop $NAME
 docker rm $NAME
 
 #RUN WITH THE jupyter/all-spark-notebook image
 docker run --name $NAME'-notebook' -d -t --net=host -p 8888:8888 -e SPARK_LOCAL_IP=$HOST_IP -v `pwd`:/home/$NAME -w /home/$NAME jupyter/all-spark-notebook
+
+echo "checkout http://"$HOST_IP":8888"
