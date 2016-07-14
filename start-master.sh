b2e5b087bec75a92be8add7222d99fcb481ceaef
Jason Shiverick <jshiverick@tesla.com>
2016-07-14 15:51:44 -0700

    updated start-master to run with only required ports

--- start-master.sh
+++ start-master.sh
@@ -2,8 +2,18 @@ NAME="spark-master"
 HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 docker stop $NAME
 docker rm $NAME
+__image="jupyter/all-spark-notebook"
 #-p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080
-docker run --name $NAME -d -t --net=host -u 0 -p 1-65535:1-65535 --expose=1-65535 -e SPARK_MASTER_IP=$HOST_IP jupyter/all-spark-notebook /bin/bash
+        #-p 1-65535:1-65535 \
+docker run \
+	--name $NAME \
+	-d -t \
+	--net=host \
+	-u 0 \
+	-p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080 \
+	--expose=1-65535 \
+	-e SPARK_MASTER_IP=$HOST_IP \
+	$__image /bin/bash
 docker exec -d $NAME /usr/local/spark/sbin/start-master.sh
 docker exec -d $NAME tail -n 1000 -F /usr/local/spark/logs/*.out
 
