6a53e44c8882c75bcb43d6ff0cea926c17a82cc5
Jason Shiverick <jshiverick@tesla.com>
2016-07-14 18:03:58 -0700

    added max_cores_per_user argument to start-master.sh

--- start-master.sh
+++ start-master.sh
@@ -3,8 +3,12 @@ HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 docker stop $NAME
 docker rm $NAME
 __image="jupyter/all-spark-notebook"
-#-p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080
-        #-p 1-65535:1-65535 \
+
+max_cores_per_user=$1
+if [ "$max_cores_per_user" == "" ]; then
+    max_cores_per_user="1000000000"
+fi
+
 docker run \
 	--name $NAME \
 	-d -t \
@@ -13,6 +17,7 @@ docker run \
 	-p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080 \
 	--expose=1-65535 \
 	-e SPARK_MASTER_IP=$HOST_IP \
+	-e SPARK_MASTER_OPTS="-Dspark.deploy.defaultCores="$max_cores_per_user \
 	$__image /bin/bash
 docker exec -d $NAME /usr/local/spark/sbin/start-master.sh
 docker exec -d $NAME tail -n 1000 -F /usr/local/spark/logs/*.out
@@ -21,8 +26,4 @@ echo "The spark ui can be seen in a web browser at: http://"$HOST_IP":8080"
 echo "run the worker on each worker node via..."
 echo "bash start-worker.sh spark://"$HOST_IP":7077"
 
-#HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
-#export SPARK_LOCAL_IP=$HOST_IP
-#export SPARK_MASTER_IP=$HOST_IP
-#/usr/share/spark/spark-1.6.0-bin-hadoop2.4/sbin/start-master.sh
 
